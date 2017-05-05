20b2ade72caa6e349291ed0248e839b0ee21e564
Adrien Maret <amaret@bertek.fr>
2017-05-05 15:10:29 +0200

    Add technos

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module ConvertMyCoin
     unless Rails.env.production?
       config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
     end
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-ApplicationController.renderer.defaults.merge!(
-  https: true
-)
+# ApplicationController.renderer.defaults.merge!(
+#   https: true
+# )
